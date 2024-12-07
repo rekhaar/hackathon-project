77ba3624d715c48e258bf0e603987f8e57562752
tdinesh <tharanga.dinesh@lseg.com>
2024-12-07 14:42:53 +0530

    added global exception handler

--- mortgage/src/main/java/com/abc/app/mortgage/service/AccountService.java
+++ mortgage/src/main/java/com/abc/app/mortgage/service/AccountService.java
@@ -7,8 +7,6 @@ import com.abc.app.mortgage.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class AccountService {
 
